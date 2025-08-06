8402068a6e376ef9f3dff9ef9dc4f22ce8de8fe6
joshua-journey-apps <joshua@journeyapps.com>
2025-08-06 11:30:05 +0200

    fix: clear text ws communication

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,13 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'PowerSync Ionic Example',
   webDir: 'www',
+  server: {
+    allowNavigation: ['*'],
+    cleartext: true, 
+  },
+  android: {
+    allowMixedContent: true,
+  },
   plugins: {
     CapacitorHttp: {
       enabled: false
